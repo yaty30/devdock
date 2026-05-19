2576e7217de1246f1d5a71d6b5dc3be5935f91fa
James <josueyat318@gmail.com>
2026-05-19 23:23:41 +0800

    update

---
+++ electron.vite.config.1779204200408.mjs
@@ -0,0 +1,30 @@
+// electron.vite.config.ts
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+var appVersion = JSON.parse(
+  readFileSync(resolve("package.json"), "utf8")
+).version;
+var electron_vite_config_default = defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: { outDir: "build/main" }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: { outDir: "build/preload" }
+  },
+  renderer: {
+    root: resolve("src/renderer"),
+    base: "./",
+    plugins: [react()],
+    define: {
+      __APP_VERSION__: JSON.stringify(appVersion)
+    },
+    build: { outDir: resolve("build/renderer") }
+  }
+});
+export {
+  electron_vite_config_default as default
+};
